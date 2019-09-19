a9b8b3d9ba22f2b6162c4f39cc0c0c73c276c9f7
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-09-19 15:06:08 +0530

    Commit the setup of node js project

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.status(200).json('Test api'));
+router.get('/', (req, res) =>
+  res
+    .status(200)
+    .json(
+      'Test api lkahsdfjk lkasdjflkdsj lksjdfkljasdfjklasd lkjsadlkfjadsklf lkjslkfjasdf lkjasdf',
+    ),
+);
 
 router.get('/:id', (req, res) => res.status(200).json(req.params.id));
 
